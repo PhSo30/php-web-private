929ea1c33484d9ad489b4e0248678ead48b45d1b
Sophea Phat <sopheaisanidiot@gmail.com>
2026-01-05 10:23:38 +0700

    add upload

--- includes/novbar.inc.php
+++ includes/novbar.inc.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid" .bg-dark.bg-gradient>
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
